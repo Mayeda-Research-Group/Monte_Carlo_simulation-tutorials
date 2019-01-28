bde8eed2fb8951772f5d03b282b37e19e311dd82
Crystal Shaw <c.shaw@ucla.edu>
2019-01-27 18:48:48 -0800

    Added comment blocks
    renamed simulation function to "collider_sim"

--- RScripts/collider_bias_sim_script.R
+++ RScripts/collider_bias_sim_script.R
@@ -1,18 +1,59 @@
-#Add comment block
+#******************************************************************************#
+# Using Using Monte Carlo Simulations for Quantitative Bias Analysis           #
+# Date                                                                         #
+# Crystal Shaw                                                                 #
+# Questions/Comments: c.shaw@ucla.edu                                          #
+#                                                                              #
+# Simulation example: Collider-stratification bias                             #
+#                                                                              #
+# Required files to run simulation:                                            #
+#   (1) collider_bias_sim_par.R:                                               #
+#       -Sets parameter inputs for the simulation                              #
+#   (2) collider_bias_sim_script.R:                                            #
+#       -Generates and analyzes data and returns results                       #
+#   (3) colider_bias_sim_run.R:                                                #
+#       -Runs simulation and stores results                                    #                                                              
+#******************************************************************************#
+
+#******************************************************************************#
+# This file generates and analyzes data and returns the parameter estimates for#
+# one iteration of sample generation.                                          #
+#******************************************************************************#
+
+#******************************************************************************#
+# Package quick reference and resources                                        #
+#                                                                              #
+# pacman: clean way to import many R packages at one time                      #
+#         (https://www.rdocumentation.org/packages/pacman/versions/0.5.0)      #
+#                                                                              #
+# here: prevents the issues associated with specifying file paths              #
+#       (https://www.rdocumentation.org/packages/here/versions/0.1)            #
+#                                                                              #
+# tidyverse: a suite of data wrangling, analysis, and visualization packages   #
+#            (https://www.rdocumentation.org/packages/tidyverse/versions/1.2.1)#
+#                                                                              #
+#******************************************************************************#
+
+#******************************************************************************#
+# A note on custom functions...                                                #
+#   It is best practice to save each user-defined function in a separate       #
+#   RScript for debugging and testing purposes. For simplicity in this tutorial# 
+#   and because the functions are so small, however, we chose to include them  #
+#   in the "#---- Custom functions ----" portion of this RScript.              #
+#******************************************************************************#
 
 #---- Package Loading and Options ----
 if (!require("pacman")) 
   install.packages("pacman", repos='http://cran.us.r-project.org')
 
-p_load("tidyverse", "MASS")
+p_load("here", "tidyverse")
 
 #Using standard notation (as opposed to scientific), rounded to three 
 #decimal places
 options(scipen = 999)
-options(digits = 2)
+options(digits = 3)
 
-#---- Defining the probability mechanisms for S and Y ----
-#Oftentimes these functions are put into separate RScripts...
+#---- Custom Functions ----
 p_S <- function(A, U){
   exp(g0 + g1*A + g2*U + g3*U*A)/(1 + exp(g0 + g1*A + g2*U + g3*U*A))
 }
@@ -22,7 +63,7 @@ p_Y <- function(A, U){
 }
 
 #---- The simulation function ----
-stroke_sim <- function(){
+collider_sim <- function(){
   #Generating the data
   #Comment about tibbles
   #Comment on each line # about what it does
